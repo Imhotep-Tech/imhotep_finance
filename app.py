ddb5a6472ed1fd17969c98b2c2db1f93463e9d98
kbassem10 <kimobisso@gmail.com>
2024-07-26 17:37:12 +0300

    fixed the login without username and pass

--- app.py
+++ app.py
@@ -30,6 +30,11 @@ mail = Mail(app)
 #connection with the database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kbassem:kb@localhost/imhotep_finance'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+SQLALCHEMY_ENGINE_OPTIONS = {
+    'pool_recycle': 3600, 
+    'pool_timeout': 30,   
+}
+
 db = SQLAlchemy(app)
 
 #define the place to save the user photo and the allowed image formats
