57bc333929282ccc638a750130cbce0f9b049dd9
kbassem10 <kimobisso@gmail.com>
2024-10-05 22:25:40 +0300

    added the error handle again and adding the new footer and version

--- app.py
+++ app.py
@@ -391,10 +391,6 @@ def get_user_data(user_id):
 def page_not_found(error):
     return render_template('error_handle.html', error_code = "404", error_description = "We can't find that page."), 404
 
-'''@app.errorhandler(500)
-def internal_server_error(error):
-    return render_template('error_handle.html', error_code = "500", error_description="Something Went Wrong."), 500'''
-
 @app.errorhandler(400)
 def session_expired(error):
     return render_template('error_handle.html', error_code = "400", error_description= "Session Expired."), 400
@@ -403,9 +399,17 @@ def session_expired(error):
 def request_amount_exceed(error):
     return render_template('error_handle.html', error_code = "429", error_description= "You exceeded the Maximum amount of requests! Please Try Again Later"), 429
 
-'''@app.errorhandler(Exception)
+@app.errorhandler(405)
+def page_not_found(error):
+    return render_template('error_handle.html', error_code = "405", error_description = "Method Not Allowed."), 405
+
+@app.errorhandler(Exception)
 def server_error(error):
-    return render_template('error_handle.html', error_code = "500", error_description = "Something went wrong."), 500'''
+    return render_template('error_handle.html', error_code = "500", error_description = "Something went wrong."), 500
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('error_handle.html', error_code = "500", error_description="Something Went Wrong."), 500
 
 @app.route("/", methods=["GET"])
 def index():
