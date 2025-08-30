015d859dba9c3f441da27649a9351b4fef2d33ea
Karim Bassem Joseph <k.bassem2397@nu.edu.eg>
2025-08-30 11:42:35 +0300

    add the target model

--- backend/imhotep_finance/finance_management/models.py
+++ backend/imhotep_finance/finance_management/models.py
@@ -111,3 +111,20 @@ class ScheduledTransaction(models.Model):
         verbose_name = "ScheduledTransaction"
         verbose_name_plural = "ScheduledTransactions"
         ordering = ['-created_at']
+
+class Target(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='target_user')
+    target = models.IntegerField()
+    month = models.IntegerField()
+    year = models.IntegerField()
+    score = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Target of {self.user.username} - {self.month}/{self.year}: {self.score}/{self.target}"
+
+    class Meta:
+        verbose_name = "Target"
+        verbose_name_plural = "Targets"
+        ordering = ['-created_at']
