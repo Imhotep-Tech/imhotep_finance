9e47334b888e54f0a21f87725ed2596d44e89a5d
Karim Bassem Joseph <k.bassem2397@nu.edu.eg>
2026-01-23 22:53:08 +0200

    feat: Implement default exchange rates for testing and enhance error handling in rate fetching

--- backend/imhotep_finance/finance_management/utils/currencies.py
+++ backend/imhotep_finance/finance_management/utils/currencies.py
@@ -3,11 +3,173 @@ import datetime
 import requests
 from django.contrib.auth import get_user_model
 from django.utils import timezone
+from django.conf import settings
 from decouple import config
 from transaction_management.models import Transactions, NetWorth
 from finance_management.models import BaseExchangeRate
 
 BASE_CURRENCY = 'USD'
+
+def get_default_test_rates():
+    """Return default exchange rates for testing when API is unavailable."""
+    # Default rates: 1 USD = 1 USD, 1 EUR = 0.92 USD, 1 GBP = 0.73 USD, etc.
+    # For testing, we'll use simple 1:1 rates for most currencies to make calculations predictable
+    return {
+        'USD': 1.0,
+        'EUR': 0.92,
+        'GBP': 0.73,
+        'JPY': 0.0067,
+        'CAD': 0.74,
+        'AUD': 0.65,
+        'CHF': 1.08,
+        'CNY': 0.14,
+        'SEK': 0.095,
+        'NZD': 0.60,
+        'EGP': 0.032,
+        'AED': 0.27,
+        'SAR': 0.27,
+        'KWD': 3.25,
+        'QAR': 0.27,
+        'BHD': 2.65,
+        'OMR': 2.60,
+        'JOD': 1.41,
+        'LBP': 0.00066,
+        'SYP': 0.00040,
+        'INR': 0.012,
+        'PKR': 0.0036,
+        'BDT': 0.0091,
+        'LKR': 0.0033,
+        'NPR': 0.0075,
+        'BTN': 0.012,
+        'AFN': 0.014,
+        'IRR': 0.000024,
+        'IQD': 0.00076,
+        'TRY': 0.034,
+        'RUB': 0.011,
+        'UAH': 0.027,
+        'PLN': 0.25,
+        'CZK': 0.043,
+        'HUF': 0.0028,
+        'RON': 0.22,
+        'BGN': 0.56,
+        'HRK': 0.14,
+        'RSD': 0.0093,
+        'MKD': 0.018,
+        'ALL': 0.010,
+        'BAM': 0.56,
+        'MDL': 0.056,
+        'GEL': 0.38,
+        'AMD': 0.0025,
+        'AZN': 0.59,
+        'KGS': 0.011,
+        'KZT': 0.0022,
+        'UZS': 0.000081,
+        'TJS': 0.091,
+        'TMT': 0.29,
+        'MNT': 0.00029,
+        'KRW': 0.00075,
+        'THB': 0.028,
+        'VND': 0.000041,
+        'LAK': 0.000048,
+        'KHR': 0.00024,
+        'MMK': 0.00048,
+        'IDR': 0.000064,
+        'MYR': 0.21,
+        'SGD': 0.74,
+        'PHP': 0.018,
+        'BND': 0.74,
+        'TWD': 0.032,
+        'HKD': 0.13,
+        'MOP': 0.12,
+        'ZAR': 0.054,
+        'BWP': 0.073,
+        'NAD': 0.054,
+        'SZL': 0.054,
+        'LSL': 0.054,
+        'ZMW': 0.040,
+        'ZWL': 0.0027,
+        'MWK': 0.00058,
+        'TZS': 0.00040,
+        'UGX': 0.00027,
+        'KES': 0.0068,
+        'RWF': 0.00080,
+        'BIF': 0.00035,
+        'DJF': 0.0056,
+        'ERN': 0.067,
+        'ETB': 0.018,
+        'SOS': 0.0018,
+        'SCR': 0.074,
+        'MUR': 0.022,
+        'MGA': 0.00022,
+        'KMF': 0.0022,
+        'AOA': 0.0012,
+        'CDF': 0.00037,
+        'XAF': 0.0016,
+        'XOF': 0.0016,
+        'XPF': 0.0091,
+        'MAD': 0.10,
+        'DZD': 0.0074,
+        'TND': 0.32,
+        'LYD': 0.21,
+        'SDG': 0.0017,
+        'SSP': 0.0067,
+        'NGN': 0.00067,
+        'GHS': 0.081,
+        'SLE': 0.040,
+        'LRD': 0.0050,
+        'GMD': 0.016,
+        'GNF': 0.00012,
+        'SLL': 0.000040,
+        'CVE': 0.0098,
+        'STN': 0.044,
+        'BRL': 0.20,
+        'ARS': 0.0012,
+        'CLP': 0.0011,
+        'COP': 0.00024,
+        'PEN': 0.27,
+        'BOB': 0.14,
+        'PYG': 0.00014,
+        'UYU': 0.025,
+        'GYD': 0.0048,
+        'SRD': 0.027,
+        'VES': 0.000028,
+        'TTD': 0.15,
+        'JMD': 0.0065,
+        'BBD': 0.50,
+        'BSD': 1.0,
+        'BZD': 0.50,
+        'GTQ': 0.13,
+        'HNL': 0.040,
+        'NIO': 0.027,
+        'CRC': 0.0019,
+        'PAB': 1.0,
+        'CUP': 0.042,
+        'HTG': 0.0074,
+        'DOP': 0.017,
+        'MXN': 0.059,
+        'XCD': 0.37,
+        'AWG': 0.56,
+        'ANG': 0.56,
+        'FJD': 0.45,
+        'PGK': 0.27,
+        'SBD': 0.12,
+        'VUV': 0.0085,
+        'WST': 0.37,
+        'TOP': 0.42,
+        'TVD': 0.65,
+        'KID': 0.65,
+        'CKD': 0.60,
+        'FKP': 0.73,
+        'GIP': 0.73,
+        'GGP': 0.73,
+        'IMP': 0.73,
+        'JEP': 0.73,
+        'SHP': 0.73,
+        'ISK': 0.0072,
+        'NOK': 0.095,
+        'DKK': 0.14,
+        'FOK': 0.14,
+    }
 
 def get_fav_currency(user):
     return getattr(user, 'favorite_currency', 'USD')
@@ -42,12 +204,35 @@ def get_or_update_rates(base_currency=BASE_CURRENCY):
                 return new_rates
             else:
                 print(f"API fetch failed, using cached rates from DB")
+                # If no cached rates and we're in test mode, use default test rates
+                if not rate_obj.rates:
+                    # Check if we're in test mode
+                    is_test_mode = (
+                        hasattr(settings, 'TESTING') or 
+                        'test' in settings.SETTINGS_MODULE.lower() or
+                        os.environ.get('DJANGO_SETTINGS_MODULE', '').endswith('settings_test')
+                    )
+                    if is_test_mode:
+                        print("Test mode detected, using default test exchange rates")
+                        default_rates = get_default_test_rates()
+                        rate_obj.rates = default_rates
+                        rate_obj.save()
+                        return default_rates
                 return rate_obj.rates if rate_obj.rates else False
         else:
             # Rates are fresh
             return rate_obj.rates if rate_obj.rates else False
     except Exception as e:
         print(f"Error getting/updating rates: {e}")
+        # In test mode, return default rates even on error
+        is_test_mode = (
+            hasattr(settings, 'TESTING') or 
+            'test' in settings.SETTINGS_MODULE.lower() or
+            os.environ.get('DJANGO_SETTINGS_MODULE', '').endswith('settings_test')
+        )
+        if is_test_mode:
+            print("Test mode detected, using default test exchange rates after error")
+            return get_default_test_rates()
         return False
 
 def convert_to_fav_currency(user, amounts_not_fav_currency):
